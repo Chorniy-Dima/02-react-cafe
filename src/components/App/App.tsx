686347f38de67b4196ea183bfd34f7198600a478
Dima <skylne162@gmail.com>
2025-11-11 17:18:08 +0200

    hw2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,44 @@
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/CafeInfo.tsx';
+import VoteOptions from '../VoteOptions/VoteOptions.tsx';
+import VoteStatus from '../VoteStats/VoteStats.tsx';
+import Notification from '../Notification/Notification.tsx';
+import { useState } from 'react';
+import type { Vote } from '../../types/votes.ts';
+import type { VoteType } from '../../types/votes.ts';
+
+export default function App() {
+  const [values, setValues] = useState<Vote>({ good: 0, neutral: 0, bad: 0 });
+  
+  const handleVote = (type: VoteType) => {
+    setValues({
+      ...values,
+      [type]: values[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setValues({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    })
+  };
+
+  const totalVotes = values.good + values.neutral + values.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((values.good / totalVotes) * 100)
+    : 0;
+  
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes? true: false} />
+        {totalVotes > 0 && <VoteStatus votes={values} totalVotes={totalVotes} positiveRate={positiveRate} />}
+        {totalVotes == 0 && <Notification/>}
+      </div>
+    </>
+  )
+}
